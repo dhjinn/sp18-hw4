99de2dfe7cd3683f68d9f801521fea6623be5c57
Derek Jin <derekjin@berkeley.edu>
2018-03-08 17:08:37 -0800

    changed last thing but finished

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180309000217) do
+ActiveRecord::Schema.define(version: 20180309010248) do
 
   create_table "cats", force: :cascade do |t|
     t.string "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20180309000217) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "age"
   end
 
 end
